255ddfaf63eea5b23dad172bd6dc22b3f9df982a
Vasilii Pronin <letstalkaboutpork@gmail.com>
2021-10-27 22:28:32 +0300

    Initial Commit

---
+++ TrafficLightV2/TrafficLightV2App.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightV2App.swift
+//  TrafficLightV2
+//
+//  Created by Василий Пронин on 27.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
